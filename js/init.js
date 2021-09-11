f9942f3346a6efff516ff21e172c939f2a4e3c02
Atharva Ikhar <atharva464@gmail.com>
2021-09-11 21:55:48 +0530

    Add files via upload

---
+++ js/init.js
@@ -0,0 +1,122 @@
+/*
+	Folio by Pixelarity
+	pixelarity.com @pixelarity
+	License: pixelarity.com/license
+*/
+
+(function($) {
+
+	skel.init({
+		reset: 'full',
+		breakpoints: {
+			'global':	{ range: '*', href: 'css/style.css', grid: { gutters: 50 } },
+			'desktop':	{ range: '737-', href: 'css/style-desktop.css', containers: 960 },
+			'1000px':	{ range: '737-1200', href: 'css/style-1000px.css', viewport: { width: 1080 } },
+			'mobile':	{ range: '-736', href: 'css/style-mobile.css', containers: '100%!', grid: { collapse: true, gutters: 20 }, viewport: { scalable: false } }
+		},
+		plugins: {
+			layers: {
+				config: {
+					mode: 'transform'
+				},
+				navPanel: {
+					hidden: true,
+					breakpoints: 'mobile',
+					position: 'top-left',
+					side: 'left',
+					animation: 'pushX',
+					width: '80%',
+					height: '100%',
+					clickToHide: true,
+					html: '<div data-action="navList" data-args="nav"></div>',
+					orientation: 'vertical'
+				},
+				titleBar: {
+					breakpoints: 'mobile',
+					position: 'top-left',
+					side: 'top',
+					height: 44,
+					width: '100%',
+					html: '<span class="toggle" data-action="toggleLayer" data-args="navPanel"></span><span class="title" data-action="copyHTML" data-args="logo"></span>'
+				}
+			}
+		}
+	});
+
+	$(function() {
+
+		var	$window = $(window);
+
+		// Forms (IE<10).
+			var $form = $('form');
+			if ($form.length > 0) {
+
+				$form.find('.form-button-submit')
+					.on('click', function() {
+						$(this).parents('form').submit();
+						return false;
+					});
+
+				if (skel.vars.IEVersion < 10) {
+					$.fn.n33_formerize=function(){var _fakes=new Array(),_form = $(this);_form.find('input[type=text],textarea').each(function() { var e = $(this); if (e.val() == '' || e.val() == e.attr('placeholder')) { e.addClass('formerize-placeholder'); e.val(e.attr('placeholder')); } }).blur(function() { var e = $(this); if (e.attr('name').match(/_fakeformerizefield$/)) return; if (e.val() == '') { e.addClass('formerize-placeholder'); e.val(e.attr('placeholder')); } }).focus(function() { var e = $(this); if (e.attr('name').match(/_fakeformerizefield$/)) return; if (e.val() == e.attr('placeholder')) { e.removeClass('formerize-placeholder'); e.val(''); } }); _form.find('input[type=password]').each(function() { var e = $(this); var x = $($('<div>').append(e.clone()).remove().html().replace(/type="password"/i, 'type="text"').replace(/type=password/i, 'type=text')); if (e.attr('id') != '') x.attr('id', e.attr('id') + '_fakeformerizefield'); if (e.attr('name') != '') x.attr('name', e.attr('name') + '_fakeformerizefield'); x.addClass('formerize-placeholder').val(x.attr('placeholder')).insertAfter(e); if (e.val() == '') e.hide(); else x.hide(); e.blur(function(event) { event.preventDefault(); var e = $(this); var x = e.parent().find('input[name=' + e.attr('name') + '_fakeformerizefield]'); if (e.val() == '') { e.hide(); x.show(); } }); x.focus(function(event) { event.preventDefault(); var x = $(this); var e = x.parent().find('input[name=' + x.attr('name').replace('_fakeformerizefield', '') + ']'); x.hide(); e.show().focus(); }); x.keypress(function(event) { event.preventDefault(); x.val(''); }); });  _form.submit(function() { $(this).find('input[type=text],input[type=password],textarea').each(function(event) { var e = $(this); if (e.attr('name').match(/_fakeformerizefield$/)) e.attr('name', ''); if (e.val() == e.attr('placeholder')) { e.removeClass('formerize-placeholder'); e.val(''); } }); }).bind("reset", function(event) { event.preventDefault(); $(this).find('select').val($('option:first').val()); $(this).find('input,textarea').each(function() { var e = $(this); var x; e.removeClass('formerize-placeholder'); switch (this.type) { case 'submit': case 'reset': break; case 'password': e.val(e.attr('defaultValue')); x = e.parent().find('input[name=' + e.attr('name') + '_fakeformerizefield]'); if (e.val() == '') { e.hide(); x.show(); } else { e.show(); x.hide(); } break; case 'checkbox': case 'radio': e.attr('checked', e.attr('defaultValue')); break; case 'text': case 'textarea': e.val(e.attr('defaultValue')); if (e.val() == '') { e.addClass('formerize-placeholder'); e.val(e.attr('placeholder')); } break; default: e.val(e.attr('defaultValue')); break; } }); window.setTimeout(function() { for (x in _fakes) _fakes[x].trigger('formerize_sync'); }, 10); }); return _form; };
+					$form.n33_formerize();
+				}
+
+			}
+
+		// Dropdowns.
+			$('#nav > ul').dropotron({
+				offsetY: -20,
+				offsetX: -2,
+				mode: 'fade',
+				speed: 300,
+				noOpenerFade: true,
+				detach: false
+			});
+
+		// Sliders.
+			var $slider = $('#slider');
+			if ($slider.length > 0) {
+
+				 $slider.slidertron({
+					mode: 'fade',
+					seamlessWrap: false,
+					viewerSelector: '.viewer',
+					speed: 'slow',
+					autoFit: true,
+					autoFitAspectRatio: (936 / 484),
+					reelSelector: '.viewer .reel',
+					slidesSelector: '.viewer .reel .slide',
+					advanceDelay: 5000,
+					speed: 'slow',
+					captionLines: 1,
+					captionLineSelector: '.caption-line',
+					slideCaptionSelector: '.caption',
+					indicatorSelector: '.indicator ul li',
+					slideLinkSelector: '.link'
+				});
+
+				$window.resize(function() {
+					 $slider.trigger('slidertron_reFit');
+				});
+
+			}
+
+		// Galleries.
+			var $gallery = $('.gallery');
+			if ($gallery.length > 0) {
+
+				$gallery.poptrox({
+					overlayClass: 'poptrox-overlay skel-layers-fixed',
+					usePopupDefaultStyling: false,
+					windowMargin: 20,
+					popupLoaderText: '',
+					popupCloserText: '',
+					baseZIndex: 20000
+				});
+
+			}
+
+	});
+
+})(jQuery);
